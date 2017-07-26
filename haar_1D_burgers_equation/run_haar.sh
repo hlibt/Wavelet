cdde5364c37c09243f12834dedfd3d8be94f95b3
Brandon Louis Gusto <blg13@dskscs012.sc.local>
2017-07-26 13:22:13 -0400

    Added more updates to code, cleaned up directory structure.

--- haar_1D_burgers_equation/run_haar.sh
+++
@@ -1,5 +0,0 @@
-g++ burger1D.cpp -o exec
-./exec
-cd output
-gnuplot animate.plt
-xdg-open animate.gif
---
+++ haar_1D_burgers_equation/src/run_haar.sh
@@ -0,0 +1,5 @@
+g++ burger1D.cpp -o exec
+./exec
+cd output/
+gnuplot animate.plt
+xdg-open animate.gif
